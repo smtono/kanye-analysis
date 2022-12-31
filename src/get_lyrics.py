5e1f8aaa811c2f495be30b3d3703b1e0dbec51c8
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-12-31 12:54:42 -0600

    start basic lyrics fetching

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -7,3 +7,9 @@ from lyricsgenius import Genius
 
 genius = Genius(access_token=os.environ['GENIUS_ACCESS_TOKEN'])
 
+artist = genius.search_artist('Kanye West')
+
+for song in artist.songs:
+    lyrics = song.lyrics
+    with open(f'data/lyrics/{song.title}.txt', 'w') as f:
+        f.write(lyrics)

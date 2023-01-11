adebc5e17058e6a0dc5235c42385900ce164bb00
tono || トノ <64560893+smtono@users.noreply.github.com>
2023-01-11 17:27:40 -0600

    lyrics fetch

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -6,26 +6,25 @@ import os
 import json
 from lyricsgenius import Genius
 
-# TODO: Get Kanye songs from JSON
 songs = []
-
-# TODO: Get songs from JSON
+json.load(open('data/kanye_songs.json', 'r'))
 
 genius = Genius(access_token=os.environ['GENIUS_ACCESS_TOKEN'])
 
 # TODO: iterate through songs and get lyrics
 # Get lyrics for all Kanye songs
-while True:
-    try:
-        # TODO: change to current song
-        song = genius.search_song('Through the Wire', 'Kanye West')
-        song.save_lyrics("kanye_lyrics")
-        
-        # TODO: Move the saved lyrics to appropriate folder (in data/lyrics)
-        
-        break
-    except:
-        pass
+for song in songs:
+    while True:
+        try:
+            # TODO: change to current song
+            song = genius.search_song(song, 'Kanye West')
+            song.save_lyrics(f"{song}_lyrics")
+            
+            # TODO: Move the saved lyrics to appropriate folder (in data/lyrics)
+            
+            break
+        except:
+            pass
 
 # TODO: Read in lyrics from JSON
 

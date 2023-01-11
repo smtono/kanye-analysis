dbfa0fe1b237b0633e07cfdfe04dc586b371714f
tono || トノ <64560893+smtono@users.noreply.github.com>
2023-01-11 15:14:52 -0600

    write pseudo

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -3,19 +3,30 @@ This script is used to scrape lyrics from all of Kanye's songs using the Genius 
 """
 
 import os
+import json
 from lyricsgenius import Genius
+
+# TODO: Get Kanye songs from JSON
+songs = []
+
+# TODO: Get songs from JSON
 
 genius = Genius(access_token=os.environ['GENIUS_ACCESS_TOKEN'])
 
+# TODO: iterate through songs and get lyrics
 # Get lyrics for all Kanye songs
 while True:
     try:
-        #artist = genius.search_artist('Kanye West')
-        #artist.save_lyrics("kanye_lyrics")
+        # TODO: change to current song
         song = genius.search_song('Through the Wire', 'Kanye West')
         song.save_lyrics("kanye_lyrics")
+        
+        # TODO: Move the saved lyrics to appropriate folder (in data/lyrics)
+        
         break
     except:
         pass
 
-# Read in lyrics from JSON
+# TODO: Read in lyrics from JSON
+
+# TODO: Save lyrics to separate file

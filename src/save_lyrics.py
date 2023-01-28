98a761a200c72f1f311bc31b1d5fc8a93ac50050
tono || トノ <64560893+smtono@users.noreply.github.com>
2023-01-27 19:05:09 -0600

    save just lyrics from json also why no album, genius?

--- src/save_lyrics.py
+++ src/save_lyrics.py
@@ -1,3 +1,8 @@
+"""
+This script is used to extract the lyrics object from the JSON objects located in src/data/lyrics
+These lyrics are then written to text files, and stored in the database after light cleaning of Genius artifacts
+"""
+
 # Save lyrics
 #  Read in lyrics from JSON
 import json
@@ -8,25 +13,31 @@ lyrics = {} # Store lyrics from json files
 
 directory = os.fsencode(os.path.join(os.getcwd(), 'src', 'data', 'lyrics'))
 
-for file in os.listdir(directory):
+for file in os.listdir(directory): # Iterate through lyrics json files
      filename = os.fsdecode(file)
      if filename.endswith(".json"):
          data = json.load(open(os.path.join(os.getcwd(), 'src', 'data', 'lyrics', filename), 'r'))
-         lyrics[filename.replace('_lyrics.json', '')] = data['lyrics'] # ?? TODO: fix
+         
+         # Clean song title
+         song_title = filename.replace('_lyrics.json', '')
+         
+         # Store Data
+         lyrics[song_title] = data['lyrics']
          continue
      else:
          continue
 
-info = json.load(open(os.path.join(os.getcwd(), 'src', 'data', 'lyrics', f'lyrics/{song}_lyrics.json)', 'r')))
-lyrics = info['lyrics']
-
-# Save lyrics to separate file
-with open(f'data/lryics/{song}_lyrics.txt', 'w') as f:
-    f.write(lyrics)
+# Saving lyrics
 
 # Database config
 connector = sqlite3.connect(os.path.join(os.getcwd(), 'src', 'data', 'lyrics.db'))
 database = connector.cursor()
+
+for key, value in lyrics.items():
+    song = key
+    lyrics = value['lyrics']
+
+    # TODO: clean lyrics
 
 # Save song, info, and lyrics to DB
 database.execute(f"INSERT INTO kanye VALUES ('{song}', '{album}', {0}, '{lyrics}')")
